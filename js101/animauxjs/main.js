7db61df94f4e98d3b824206835e8817da82c899b
orkeilius <orkeilius@users.noreply.github.com>
2022-12-14 15:46:48 +0100

    init animaux_js commit

---
+++ js101/animauxjs/main.js
@@ -0,0 +1,9 @@
+var animaux = ["alligator","baleine-bosse","buffle","cheval","chimpanze","elephant","lion"]
+
+function creationBoutons(){
+ //pour chaque animal, créer un bouton
+      
+}
+function changerAnimal(animal){
+    // mettre à jour la source de la photo et du player
+}
