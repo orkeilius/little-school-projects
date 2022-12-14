59132620348033a371b67446c2dccb08accfd521
orkeilius <orkeilius@users.noreply.github.com>
2022-12-14 16:33:53 +0100

    made animauxJs

--- js101/animauxjs/main.js
+++ js101/animauxjs/main.js
@@ -1,9 +1,20 @@
-var animaux = ["alligator","baleine-bosse","buffle","cheval","chimpanze","elephant","lion"]
+var animaux = ["alligator", "baleine", "buffle", "cheval", "chimpanze", "elephant", "lion"]
 
-function creationBoutons(){
- //pour chaque animal, créer un bouton
-      
+function creationBoutons() {
+    //pour chaque animal, créer un bouton
+
+    buttonParent = document.getElementById("lesAnimaux");
+    animaux.forEach(animal => {
+        button = document.createElement("button");
+        button.innerHTML = animal;
+        button.id = animal;
+        button.setAttribute("onclick", "changerAnimal('" + animal + "')")
+        buttonParent.appendChild(button);
+    });
 }
-function changerAnimal(animal){
+function changerAnimal(animal) {
     // mettre à jour la source de la photo et du player
+    console.log(animal)
+    document.getElementById("player").setAttribute("src", "cris/"+animal + ".mp3")
+    document.getElementById("photo").setAttribute("src", "photos/"+animal + ".jpeg")
 }
